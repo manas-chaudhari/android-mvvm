7a81a832e7a8f0045e5138e9aa732b8986bc6c8d
Manas Chaudhari <chaudhari.manas@gmail.com>
2016-09-02 16:18:15 +0530

    Implement RadioGroup two way binding

--- sample/src/main/java/com/manaschaudhari/android_mvvm/sample/calculator_example/CalculatorActivity.java
+++ sample/src/main/java/com/manaschaudhari/android_mvvm/sample/calculator_example/CalculatorActivity.java
@@ -17,8 +17,6 @@
 package com.manaschaudhari.android_mvvm.sample.calculator_example;
 
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.manaschaudhari.android_mvvm.MvvmActivity;
 import com.manaschaudhari.android_mvvm.ViewModel;
