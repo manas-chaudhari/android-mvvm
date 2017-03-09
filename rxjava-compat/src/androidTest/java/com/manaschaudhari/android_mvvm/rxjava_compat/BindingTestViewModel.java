7159e882c9c8252eb7a936b8042bbf299f8377da
Manas Chaudhari <chaudhari.manas@gmail.com>
2017-03-09 12:13:00 +0530

    Implement RxJava compatibility module

---
+++ rxjava-compat/src/androidTest/java/com/manaschaudhari/android_mvvm/rxjava_compat/BindingTestViewModel.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016 Manas Chaudhari
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.manaschaudhari.android_mvvm.rxjava_compat;
+
+import com.manaschaudhari.android_mvvm.ViewModel;
+import com.manaschaudhari.android_mvvm.adapters.ViewProvider;
+
+import java.util.List;
+
+import rx.Observable;
+
+public class BindingTestViewModel implements ViewModel {
+    public List<EmptyViewModel> subclassList;
+    public List<ViewModel> mixedList;
+    public Observable<List<ViewModel>> observableMixedList;
+    public Observable<List<EmptyViewModel>> observableSubclassMixedList;
+
+    public ViewProvider viewProvider;
+
+    public static class EmptyViewModel implements ViewModel {
+
+    }
+}
---
+++ rxjava-compat/src/androidTest/java/com/manaschaudhari/android_mvvm/rxjava_compat/DummyTest.java
@@ -0,0 +1,16 @@
+package com.manaschaudhari.android_mvvm.rxjava_compat;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Minimum one test is necessary for build to pass
+ */
+@RunWith(AndroidJUnit4.class)
+public class DummyTest {
+    @Test
+    public void dummy() throws Exception {
+    }
+}
